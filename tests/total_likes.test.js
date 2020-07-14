42bfb81df7c8da85e144c460018e39c43a59e6ee
jim4067 <jimmyimpulse2@gmail.com>
2020-07-14 17:05:24 +0300

    finished 4.4: helper functions and unit tests, step2

--- tests/total_likes.test.js
+++ tests/total_likes.test.js
@@ -1,20 +1,4 @@
-const dummy = (blogs) => {
-    blogs = 1;
-    return blogs;
-}
-
-const total_likes = (blogs) => {
-    const likes = blogs.map((blog_param) => blog_param.likes);
-    //console.log(likes)
-    return likes.reduce((cur_val, acc) => cur_val + acc )
-}
-
-
-module.exports = {
-    dummy,
-    total_likes
-}
-
+const total_likes = require('../utils/list_helper').total_likes;
 
 const blogs = [
     { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
@@ -23,5 +7,27 @@ const blogs = [
     { _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10, __v: 0 },
     { _id: "5a422ba71b54a676234d17fb", title: "TDD harms architecture", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", likes: 0, __v: 0 },
     { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 }
-]
+];
 
+const list_with_one_blog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ];
+
+describe("total likes", () => {
+
+      test("when the list has only one blog the likes", () => {
+          const result = total_likes(list_with_one_blog);
+          expect(result).toBe(5);
+      });
+      test("with the list of blogs above", () => {
+          const result = total_likes(blogs);
+          expect(result).toBe(36);
+      });
+});
