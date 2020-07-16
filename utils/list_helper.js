88663cf08141475ff1f8d21b61ecba36831cb1a8
jim4067 <jimmyimpulse2@gmail.com>
2020-07-16 19:32:11 +0300

    finished 4.5*: helper functions and unit tests, step3. Most favorite blog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,23 +5,21 @@ const dummy = (blogs) => {
 
 const total_likes = (blogs) => {
     const likes = blogs.map((blog_param) => blog_param.likes);
-    //console.log(likes)
     return likes.reduce((cur_val, acc) => cur_val + acc )
+}
+
+function favorite_blog(blogs) {
+    const favorite =  blogs.reduce((max, cur_val) => max.likes > cur_val.likes ? max : cur_val, blogs[0]);
+    return {
+        "title" : favorite.title,
+        "author" : favorite.author,
+        "likes" :favorite.likes
+    };
 }
 
 
 module.exports = {
     dummy,
-    total_likes
+    total_likes,
+    favorite_blog
 }
-
-
-const blogs = [
-    { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
-    { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 },
-    { _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12, __v: 0 },
-    { _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10, __v: 0 },
-    { _id: "5a422ba71b54a676234d17fb", title: "TDD harms architecture", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", likes: 0, __v: 0 },
-    { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 }
-]
-
