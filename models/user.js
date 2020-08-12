4e3df29cb45d56dff0a39ec5dda17067beca6533
jim4067 <jimmyimpulse2@gmail.com>
2020-08-12 17:31:38 +0300

    the users models

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const unique_validator = require('mongoose-unique-validator');
+
+const user_schema = mongoose.Schema({
+    username: {
+        type: String,
+        unique : true
+    },
+    name : String,
+    password_hash : String,
+    blogs : [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref : 'Blog'
+        }
+    ]
+});
+
+user_schema.plugin(unique_validator);
+
+user_schema.set('toJSON', {
+    transform : (document, returned_object) => {
+        returned_object.id = returned_object._id.toString();
+        delete returned_object._id;
+        delete returned_object.__v;
+        delete returned_object.password_hash;
+    }
+});
+
+const User = mongoose.model('User' , user_schema );
+
+module.exports = User;
