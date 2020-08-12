3b4ddd4757ddaed982c606bd33811a08a52d126d
jim4067 <jimmyimpulse2@gmail.com>
2020-08-12 17:32:33 +0300

    modified the routes for the blogs:

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,32 +1,44 @@
 const Blog = require('../models/blog');
 const blog_router = require('express').Router();
 
-blog_router.get('/', (req, res, next) => {
+blog_router.get('/', async (req, res) => {
     const fetched_blogs = await Blog.find({});
     res.json(fetched_blogs);
 });
 
-blog_router.get('/:id', (req, res, next) => {
+blog_router.get('/:id', async (req, res) => {
     const id = req.params.id;
 
     const found_blog = await Blog.findById(id);
-    if(found_blog){
+    if (found_blog) {
         res.json(found_blog);
     } else {
         res.status(404).end();
     }
 });
 
-blog_router.post('/', (req, res, next) => {
+blog_router.post('/', async (req, res) => {
     const blog = new Blog(req.body);
 
     const saved_blog = await blog.save();
     res.status(201).json(saved_blog);
 });
 
-blog_router.delete('/:id', async (req,res) => {
+blog_router.put('/:id', async (req, res) => {
+    const body = req.body;
     const id = req.params.id;
-    
+
+    const blog_to_change = {
+        likes: body.title
+    };
+
+    const updated_blog = await Blog.findByIdAndUpdate(id, blog_to_change, { new: true });
+    res.json(updated_blog);
+});
+
+blog_router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
     await Blog.findByIdAndDelete(id);
     res.json(204).end();
 });
