6aaa675fec951186cff94455ee3c4812b5120c4c
jim4067 <jimmyimpulse2@gmail.com>
2020-08-12 17:31:03 +0300

    the routes for the users

---
+++ controllers/users.js
@@ -0,0 +1,28 @@
+
+const bcrypt = require('bcrypt'); 
+const user_router = require("express").Router();
+const User = require('../models/user');
+
+user_router.get('/', async(req, res) => {
+    const users = await User.find({});
+    res.json(users);
+});
+
+user_router.post('/', async (req, res) => {
+    const body = req.body;
+
+    const salt_rounds = 10;
+    const password_hash = await bcrypt.hash(body.password, salt_rounds);
+
+    const user = new User({
+        username : body.username,
+        name : body.name,
+        password_hash
+    });
+
+    const saved_user = await user.save();
+
+    res.json(saved_user);
+});
+
+module.exports = user_router;
