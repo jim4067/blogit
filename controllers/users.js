4e3c04b32afaa06e9a0b400b820b5e65c79cb68c
jim4067 <jimmyimpulse2@gmail.com>
2020-08-14 07:56:48 +0300

    formatted the file

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,13 @@
 
-const bcrypt = require('bcrypt'); 
+const bcrypt = require('bcrypt');
 const user_router = require("express").Router();
 const User = require('../models/user');
 
-user_router.get('/', async(req, res) => {
-    const users = await User.find({})
-                            .populate('blogs');
+user_router.get('/', async (req, res) => {
+    const users = await User
+        .find({})
+        .populate('blogs');
+        
     res.json(users);
 });
 
@@ -16,8 +18,8 @@ user_router.post('/', async (req, res) => {
     const password_hash = await bcrypt.hash(body.password, salt_rounds);
 
     const user = new User({
-        username : body.username,
-        name : body.name,
+        username: body.username,
+        name: body.name,
         password_hash
     });
 
